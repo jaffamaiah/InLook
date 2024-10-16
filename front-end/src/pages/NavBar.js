3c4e9c175f4ae6e3f25ca772a52d1cea5790c011
Tomas Mejia <74030992+Tomas-Domas@users.noreply.github.com>
2024-10-16 15:47:40 -0400

    Fix port still starting from 5000 (#10)

--- front-end/src/pages/NavBar.js
+++ front-end/src/pages/NavBar.js
@@ -11,6 +11,9 @@ const Layout = () => {
           <li>
             <Link to="/people">People</Link>
           </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
       </nav>
 
