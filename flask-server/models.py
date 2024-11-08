b9dd8adb746d1c1fb89b55c8c26e646b7c9dd527
Arman <armanchowdhury@ufl.edu>
2024-11-08 00:08:41 -0500

    Specify table name 'users'

--- flask-server/models.py
+++ flask-server/models.py
@@ -30,6 +30,7 @@ class Journal(db.Model):
 
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String, nullable=False, unique=True)
