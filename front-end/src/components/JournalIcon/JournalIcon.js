633beec5f66cacd1692e705834b544c3ffaf4755
Tomas Mejia <74030992+BigDomas@users.noreply.github.com>
2024-11-18 10:06:05 -0500

    Add emotion color to icons in My Journals

--- front-end/src/components/JournalIcon/JournalIcon.js
+++ front-end/src/components/JournalIcon/JournalIcon.js
@@ -1,13 +1,22 @@
 import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 import './JournalIcon.css'
+import { emotions } from '../../utilities'
 
 
 export default function JournalIcon(args) {
     const navigate = useNavigate()
+    const [color, setColor] = useState('white')
+
+    useEffect(() => {
+        if (emotions[args.emotion]) {
+            setColor(emotions[args.emotion].color)
+        }
+    }, [args])
 
     return (
-        <div className='journal-icon' onClick={() => navigate(args.path)}>
+        <div className='journal-icon' style={{backgroundColor:color}} onClick={() => navigate(args.path)}>
             <h3>{args.date}</h3>
             <h3>{args.title}</h3>
             <h3>{args.emotion}</h3>
