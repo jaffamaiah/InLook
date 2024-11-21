89cf1334432169d5478cf31258c7426c5625b828
Tomas Mejia <74030992+Tomas-Domas@users.noreply.github.com>
2024-11-21 13:14:47 -0500

    Add Account settings page, Customize html header styles (#23)
    * Add settings page
    
    * Revert color change
    
    * Add placeholder changeProfilePicture function

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,15 +1,17 @@
 import Home from './Home/Home'
 import LogIn from './LogIn/LogIn'
 import Write from './Write/Write'
-import MyJournal from './MyJournal/MyJournal'
 import Read from './Read/Read'
+import MyJournal from './MyJournal/MyJournal'
+import Account from './Account/Account'
 import Page404 from './Page404/Page404'
 
 export {
     Home,
     LogIn,
     Write,
-    MyJournal,
     Read,
-    Page404
+    MyJournal,
+    Account,
+    Page404,
 }
