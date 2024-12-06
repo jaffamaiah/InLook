4c7a04f58b0c6cbbb8ea2b16bc4c528a04ee3ad0
Tomas Mejia <74030992+BigDomas@users.noreply.github.com>
2024-12-06 12:34:35 -0500

    Remove emotion text from journal icon

--- front-end/src/components/JournalIcon/JournalIcon.js
+++ front-end/src/components/JournalIcon/JournalIcon.js
@@ -18,6 +18,5 @@ export default function JournalIcon(args) {
     return <div className='journal-icon' style={{ backgroundColor: color }} onClick={() => navigate(args.path)}>
         <h3>{args.date}</h3>
         <h3>{args.title}</h3>
-        <h3>{args.emotion}</h3>
     </div>
 }
